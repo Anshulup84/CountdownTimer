6daadd6021faa32f8c82f63a423b24016347b08b
Anshul <avenger720720@gmail.com>
2025-04-17 16:53:41 +0530

    completed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const hours = document.getElementById('hours');
 const mins = document.getElementById('mins');
 const secs = document.getElementById('secs');
 
+const formatTime = (time) => {
+    return time < 10 ? `0${time}` : time
+}
 
 const updateCountDown = (deadline) => {
     const currentTime = new Date();
@@ -10,6 +13,18 @@ const updateCountDown = (deadline) => {
 
     
     // calculate days, hours, mins ,ses from timeDifference
+
+    let calSec = Math.floor(timeDifference/1000) % 60 ;
+    let calMin = Math.floor(timeDifference/1000/60) % 60;
+    let calHours = Math.floor(timeDifference/1000/60/60) % 24;
+    let calDays = Math.floor(timeDifference/1000/60/60/24)
+
+        days.textContent = formatTime(calDays);
+        mins.textContent = formatTime(calMin)
+        hours.textContent = formatTime(calHours)
+        secs.textContent = formatTime(calSec);
+
+        
 }
 
 const countDown = (targetDate) => {
